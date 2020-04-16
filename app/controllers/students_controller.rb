34d038113975a6b78a72910252d24a6f56619f9b
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-16 22:33:10 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,15 @@ class StudentsController < ApplicationController
   end
   
   def activate
-   binding.pry
+  # binding.pry
+    puts "reached the activate method"
+    @student = set_student
+    puts "Student = #{@student}"
+    puts "Current status is #{@student.active}"
+    @student.active = !@student.active 
+    puts "after !@student.active is run = #{@student.active}"
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
